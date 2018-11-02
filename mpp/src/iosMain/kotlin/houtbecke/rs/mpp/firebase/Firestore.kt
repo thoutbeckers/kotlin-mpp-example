bf65806f4dba6b3c6edb1945dc794c25a22f8ab2
Tijl Houtbeckers <tijl@houtbecke.rs>
2018-11-02 23:29:19 +0100

    stub iOS implementation of Firebase

--- mpp/src/iosMain/kotlin/houtbecke/rs/mpp/firebase/Firestore.kt
+++ mpp/src/iosMain/kotlin/houtbecke/rs/mpp/firebase/Firestore.kt
@@ -1,7 +1,11 @@
 package houtbecke.rs.mpp.firebase
 
 class IOSFirestore: FirestoreMPP {
-    override fun collection(path: String): CollectionReferenceMPP {
-        throw Exception("Not implemented")
+    override fun document(documentPath: String): DocumentReferenceMPP {
+        TODO("not implemented")
+    }
+
+    override fun collection(collectionPath: String): CollectionReferenceMPP {
+        TODO("not implemented")
     }
 }
